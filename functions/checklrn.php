3215c6f1a30e29b2c6e204d0019dc276751fcacc
jhayramirez <jhayramirez014@gmail.com>
2023-01-28 18:27:13 +0800

    small success and exist message

--- functions/checklrn.php
+++ functions/checklrn.php
@@ -4,10 +4,10 @@ if (!empty($_POST["IDnum"])) {
   $query = mysqli_query($conn, "SELECT * FROM students WHERE StudentID='" . $_POST["IDnum"] . "'");
   $count = mysqli_num_rows($query);
   if ($count > 0) {
-    echo "<span style='color:red'>LRN already exist .</span>";
+    echo "<small><span style='color:red'>LRN already exist .</span></small>";
     echo "<script>$('#submit').prop('disabled',true);</script>";
   } else {
-    echo "<span style='color:green'>LRN available.</span>";
+    echo "<small><span style='color:green'>LRN available.</span></small>";
     echo "<script>$('#submit').prop('disabled',false);</script>";
   }
 }
